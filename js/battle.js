1e07094a655b85ba7c69c38a916acf2709b461e8
Stijn <99053054@mydavinci.nl>
2022-03-18 23:36:45 +0100

    Final commit

--- js/battle.js
+++ js/battle.js
@@ -5,28 +5,30 @@ var energy_type_1 = require("./energy_type");
 var pokemon_1 = require("./pokemon");
 var resistance_1 = require("./resistance");
 var weakness_1 = require("./weakness");
-var Lightning = new energy_type_1.EnergyType("Lightning");
-var Fire = new energy_type_1.EnergyType("Fire");
-var Fighting = new energy_type_1.EnergyType("Fighting");
-var Pikachu = new pokemon_1.Pokemon("Pikachu", 80, Lightning, [
-    new attack_1.Attack("Electric Ring", Lightning, 50),
-    new attack_1.Attack("Pika Punch", Fighting, 20),
+var Lightning = new energy_type_1.EnergyType('Lightning');
+var Fire = new energy_type_1.EnergyType('Fire');
+var Fighting = new energy_type_1.EnergyType('Fighting');
+var Water = new energy_type_1.EnergyType('Water');
+var Pikachu = new pokemon_1.Pokemon('Pikachu', 80, Lightning, [
+    new attack_1.Attack('Electric Ring', Lightning, 50),
+    new attack_1.Attack('Pika Punch', Fighting, 20),
 ], new weakness_1.Weakness(Fire.name, 1.5), new resistance_1.Resistance(Fighting.name, 20));
-var Charmander = new pokemon_1.Pokemon("Charmander", 60, Fire, [new attack_1.Attack("Head Butt", Fighting, 10), new attack_1.Attack("Flare", Fire, 30)], new weakness_1.Weakness("Water", 2), new resistance_1.Resistance(Lightning.name, 10));
+var Charmander = new pokemon_1.Pokemon('Charmander', 60, Fire, [new attack_1.Attack('Head Butt', Fighting, 10), new attack_1.Attack('Flare', Fire, 30)], new weakness_1.Weakness(Water.name, 2), new resistance_1.Resistance(Lightning.name, 10));
+var Piplup = new pokemon_1.Pokemon('Piplup', 100, Water, [new attack_1.Attack('Tackle', Fighting, 10), new attack_1.Attack('Water Splash', Water, 30)], new weakness_1.Weakness(Lightning.name, 1.5), new resistance_1.Resistance(Fire.name, 15));
 var lastPokemon;
-var alivePokemon = [Pikachu, Charmander];
-while (alivePokemon.length > 1) {
-    if (!lastPokemon || lastPokemon === Charmander) {
-        Pikachu.Attack(Pikachu.attacks[Math.floor(Math.random() * ((Pikachu.attacks.length - 1) - 0 + 1)) + 0], Charmander);
-        lastPokemon = Pikachu;
-    }
-    else {
-        Charmander.Attack(Charmander.attacks[Math.floor(Math.random() * ((Charmander.attacks.length - 1) - 0 + 1)) + 0], Pikachu);
-        lastPokemon = Charmander;
-    }
-    alivePokemon.forEach(function (Pokemon) {
-        if (!Pokemon.alive) {
-            alivePokemon = alivePokemon.slice(alivePokemon.indexOf(Pokemon), 1);
+var pokemons = [Pikachu, Charmander, Piplup];
+while (pokemons.length > 1) {
+    pokemons.forEach(function (pokemon, index) {
+        if (!pokemon.alive) {
+            pokemons.splice(index, 1);
+        }
+        else {
+            if (index === pokemons.length - 1) {
+                pokemon.Attack(pokemon.attacks[Math.floor(Math.random() * (pokemon.attacks.length - 1 - 0 + 1)) + 0], pokemons[0]);
+            }
+            else {
+                pokemon.Attack(pokemon.attacks[Math.floor(Math.random() * (pokemon.attacks.length - 1 - 0 + 1)) + 0], pokemons[index + 1]);
+            }
         }
     });
 }
--- ts/battle.ts
+++ ts/battle.ts
@@ -1,54 +1,66 @@
-import { Attack } from "./attack";
-import { EnergyType } from "./energy_type";
-import { Pokemon } from "./pokemon";
-import { Resistance } from "./resistance";
-import { Weakness } from "./weakness";
+import { Attack } from './attack'
+import { EnergyType } from './energy_type'
+import { Pokemon } from './pokemon'
+import { Resistance } from './resistance'
+import { Weakness } from './weakness'
 
-const Lightning = new EnergyType("Lightning");
-const Fire = new EnergyType("Fire");
-const Fighting = new EnergyType("Fighting");
-
+const Lightning = new EnergyType('Lightning');
+const Fire = new EnergyType('Fire');
+const Fighting = new EnergyType('Fighting');
+const Water = new EnergyType('Water');
 
 const Pikachu = new Pokemon(
-    "Pikachu",
-    80,
-    Lightning,
-    [
-      new Attack("Electric Ring", Lightning, 50),
-      new Attack("Pika Punch", Fighting, 20),
-    ],
-    new Weakness(Fire.name, 1.5),
-    new Resistance(Fighting.name, 20)
-  );
-  const Charmander = new Pokemon(
-    "Charmander",
-    60,
-    Fire,
-    [new Attack("Head Butt", Fighting, 10), new Attack("Flare", Fire, 30)],
-    new Weakness("Water", 2),
-    new Resistance(Lightning.name, 10)
-  );
+  'Pikachu',
+  80,
+  Lightning,
+  [
+    new Attack('Electric Ring', Lightning, 50),
+    new Attack('Pika Punch', Fighting, 20),
+  ],
+  new Weakness(Fire.name, 1.5),
+  new Resistance(Fighting.name, 20),
+)
+const Charmander = new Pokemon(
+  'Charmander',
+  60,
+  Fire,
+  [new Attack('Head Butt', Fighting, 10), new Attack('Flare', Fire, 30)],
+  new Weakness(Water.name, 2),
+  new Resistance(Lightning.name, 10),
+)
 
-var lastPokemon: Pokemon;
-var alivePokemon: Pokemon[] = [Pikachu, Charmander];
+const Piplup = new Pokemon(
+  'Piplup',
+  100,
+  Water,
+  [new Attack('Tackle', Fighting, 10), new Attack('Water Splash', Water, 30)],
+  new Weakness(Lightning.name, 1.5),
+  new Resistance(Fire.name, 15)
+)
 
+var lastPokemon: Pokemon[]
+var pokemons: Pokemon[] = [Pikachu, Charmander, Piplup]
 
-
-
-
-
-while (alivePokemon.length > 1) {
-    if(!lastPokemon){
-        alivePokemon[0].Attack(alivePokemon[0].attacks[Math.floor(Math.random() * ((alivePokemon[0].attacks.length - 1) - 0 + 1)) + 0], alivePokemon[(alivePokemon.indexOf(lastPokemon) + 1)]);
-        lastPokemon = alivePokemon[0];
-    } else if(){
-        Charmander.Attack(Charmander.attacks[Math.floor(Math.random() * ((Charmander.attacks.length - 1) - 0 + 1)) + 0], Pikachu);
-        lastPokemon = Charmander;
+while (pokemons.length > 1) {
+  pokemons.forEach((pokemon, index) => {
+    if (!pokemon.alive) {
+      pokemons.splice(index, 1)
+    } else {
+      if (index === pokemons.length - 1) {
+        pokemon.Attack(
+          pokemon.attacks[
+            Math.floor(Math.random() * (pokemon.attacks.length - 1 - 0 + 1)) + 0
+          ],
+          pokemons[0],
+        )
+      } else {
+        pokemon.Attack(
+          pokemon.attacks[
+            Math.floor(Math.random() * (pokemon.attacks.length - 1 - 0 + 1)) + 0
+          ],
+          pokemons[index + 1],
+        )
+      }
     }
-    alivePokemon.forEach(Pokemon => {
-        if(!Pokemon.alive){
-            alivePokemon = alivePokemon.slice(alivePokemon.indexOf(Pokemon), 1);
-        }
-    });
-    
+  })
 }
